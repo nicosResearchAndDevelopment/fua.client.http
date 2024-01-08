f7f16030bf784322365b7a38f0a823cd89a0c213
Simon Petrac <pfoerdie@gmail.com>
2024-01-08 16:22:32 +0100

    initial commit

---
+++ src/http.js
@@ -0,0 +1,12 @@
+const
+    assert    = require('@nrd/fua.core.assert'),
+    is        = require('@nrd/fua.core.is'),
+    nodeFetch = require('node-fetch');
+
+class HTTPClient {
+
+    // TODO
+
+}
+
+module.exports = HTTPClient;
---
+++ test/http.test.js
@@ -0,0 +1,14 @@
+const
+    expect           = require('expect'),
+    {describe, test} = require('mocha'),
+    HTTPClient       = require('../src/http.js');
+
+describe('fua.client.http', function () {
+
+    test('develop', function () {
+
+        console.log(HTTPClient);
+
+    });
+
+});
