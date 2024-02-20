f7a70c21efe54b4aa6ebb10b6b699f5a60c17a06
Simon Petrac <pfoerdie@gmail.com>
2024-02-20 11:25:01 +0100

    added methods with default-client

--- src/http.js
+++ src/http.js
@@ -1,8 +1,19 @@
 const
-    model = require('./model.js');
+    model         = require('./model.js'),
+    defaultClient = new model.RequestClient();
 
 function HTTP(options) {
     return new model.RequestClient(options);
 }
+
+HTTP.fetch  = (url, options) => defaultClient.fetch(url, options);
+HTTP.get    = (url, headers) => defaultClient.get(url, headers);
+HTTP.head   = (url, headers) => defaultClient.head(url, headers);
+HTTP.post   = (url, headers, body) => defaultClient.post(url, headers, body);
+HTTP.put    = (url, headers, body) => defaultClient.put(url, headers, body);
+HTTP.delete = (url, headers, body) => defaultClient.delete(url, headers, body);
+// HTTP.options = defaultClient.options.bind(defaultClient);
+// HTTP.connect = defaultClient.connect.bind(defaultClient);
+// HTTP.trace   = defaultClient.trace.bind(defaultClient);
 
 module.exports = HTTP;
