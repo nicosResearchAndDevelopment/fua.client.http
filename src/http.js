5b15fcf1565a7afd14b674abf41f45b96d0538e1
Simon Petrac <pfoerdie@gmail.com>
2024-02-20 11:57:05 +0100

    improved main methods and added jsdocs

--- src/http.js
+++ src/http.js
@@ -6,14 +6,39 @@ function HTTP(options) {
     return new model.RequestClient(options);
 }
 
-HTTP.fetch  = (url, options) => defaultClient.fetch(url, options);
-HTTP.get    = (url, headers) => defaultClient.get(url, headers);
-HTTP.head   = (url, headers) => defaultClient.head(url, headers);
-HTTP.post   = (url, headers, body) => defaultClient.post(url, headers, body);
-HTTP.put    = (url, headers, body) => defaultClient.put(url, headers, body);
-HTTP.delete = (url, headers, body) => defaultClient.delete(url, headers, body);
-// HTTP.options = defaultClient.options.bind(defaultClient);
-// HTTP.connect = defaultClient.connect.bind(defaultClient);
-// HTTP.trace   = defaultClient.trace.bind(defaultClient);
+/** @typedef {string | URL} RequestTarget */
+/** @typedef {'GET' | 'HEAD' | 'POST' | 'PUT' | 'DELETE' | 'CONNECT' | 'OPTIONS' | 'TRACE'} RequestMethod */
+/** @typedef {Record<string, string>} RequestHeaders */
+/** @typedef {Blob | ArrayBuffer | TypedArray | DataView | FormData | URLSearchParams | string | String | ReadableStream | Readable | Dataset} RequestBody */
+/** @typedef {'cors' | 'no-cors' | 'same-origin' | 'navigate' | 'websocket'} RequestMode */
+/** @typedef {'omit' | 'same-origin' | 'include'} RequestCredentials */
+/** @typedef {'default' | 'no-store' | 'reload' | 'no-cache' | 'force-cache' | 'only-if-cached'} RequestCache */
+/** @typedef {'follow' | 'error' | 'manual'} RequestRedirect */
+/** @typedef {string | 'about:client' | ''} RequestReferrer */
+/** @typedef {'no-referrer' | 'no-referrer-when-downgrade' | 'same-origin' | 'origin' | 'strict-origin' | 'origin-when-cross-origin' | 'strict-origin-when-cross-origin' | 'unsafe-url'} RequestReferrerPolicy */
+/** @typedef {string} RequestIntegrity */
+/** @typedef {'high' | 'low' | 'auto'} RequestPriority */
+/** @typedef {'audio' | 'audioworklet' | 'document' | 'embed' | 'font' | 'frame' | 'iframe' | 'image' | 'manifest' | 'object' | 'paintworklet' | 'report' | 'script' | 'sharedworker' | 'style' | 'track' | 'video' | 'worker' | 'xslt' | ''} RequestDestination */
+/** @typedef {{method?: RequestMethod, headers?: Headers | RequestHeaders, body?: RequestBody, mode?: RequestMode, credentials?: RequestCredentials, cache?: RequestCache, redirect?: RequestRedirect, referrer?: RequestReferrer, referrerPolicy?: RequestReferrerPolicy, integrity?: RequestIntegrity, keepalive?: boolean, signal?: AbortSignal, priority?: RequestPriority}} RequestOptions */
+
+Object.defineProperties(HTTP, {
+    /** @type {function (instance: unknown): instance is RequestClient}*/
+    [Symbol.hasInstance]: {value: (instance) => (instance instanceof model.RequestClient)},
+    /** @type {function (url: RequestTarget, options?: RequestOptions): AsyncRequest} */
+    fetch: {value: defaultClient.fetch.bind(defaultClient)},
+    /** @type {function (url: RequestTarget, headers?: RequestHeaders): AsyncRequest} */
+    get: {value: defaultClient.get.bind(defaultClient)},
+    /** @type {function (url: RequestTarget, headers?: RequestHeaders): AsyncRequest} */
+    head: {value: defaultClient.head.bind(defaultClient)},
+    /** @type {function (url: RequestTarget, headers?: RequestHeaders, body?: RequestBody): AsyncRequest} */
+    post: {value: defaultClient.post.bind(defaultClient)},
+    /** @type {function (url: RequestTarget, headers?: RequestHeaders, body?: RequestBody): AsyncRequest} */
+    put: {value: defaultClient.put.bind(defaultClient)},
+    /** @type {function (url: RequestTarget, headers?: RequestHeaders, body?: RequestBody): AsyncRequest} */
+    delete: {value: defaultClient.delete.bind(defaultClient)}
+    // options: {value: defaultClient.options.bind(defaultClient)},
+    // connect: {value: defaultClient.connect.bind(defaultClient)},
+    // trace:   {value: defaultClient.trace.bind(defaultClient)},
+});
 
 module.exports = HTTP;
